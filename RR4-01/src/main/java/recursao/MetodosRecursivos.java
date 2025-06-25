e20694bc2f7dbd21d9d23c9fc484aca8d4856b85
Isadora Lucena <isadorabeatriz595@gmail.com>
2025-06-24 23:08:39 -0300

    feat: adiciona implementação do RR4-01

--- RR4-01/src/main/java/recursao/MetodosRecursivos.java
+++ RR4-01/src/main/java/recursao/MetodosRecursivos.java
@@ -2,69 +2,75 @@ package recursao;
 
 public class MetodosRecursivos {
 
-	public int calcularSomaArray(int[] array){
+	private int calcularSomaArray(int[] array, int indexFrom) {
 		int result = 0;
-		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR A SOMA
-		// DOS EMENTOS DE UM ARRAY
+		if (indexFrom < array.length) {
+			result = array[indexFrom] + calcularSomaArray(array, indexFrom + 1);
+		}
+
 		return result;
+    }
+
+	public int calcularSomaArray(int[] array){
+		return calcularSomaArray(array, 0);
 	}
+
 	public long calcularFatorial(int n) {
 		long result = 1;
-		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR E IMPRIMIR
-		// O FATORIAL DO PARAMETRO
-		// DE ACORDO COM O QUE FOI MOSTRADO NO EXERCCICIO. OBSERVE QUE SENDO O
-		// METODO
-		// RECURSIVO, O FATORIAL DOS NUMEROS ANTERIORES TAMBEM VAO SER IMPRESSOS
+		if (n > 1){
+			result = n * calcularFatorial(n-1);
+		}
+	
 		return result;
 	}
 
 	public int calcularFibonacci(int n) {
 		int result = 1;
-		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR E IMPRIMIR
-		// O N-ESIMO TERMO
-		// DA SEQUENCIA DE FIBONACCI, QUE TEM A SEGUINTE LEI DE FORMACAO: O
-		// PRIMEIRO E SEGUNDO NUMEROS
-		// DA SEQUENCIA SÃO 1. A PARTIR DO TERCEIRO TERMO, CADA TERMO DA
-		// SEQUENCIA É DADO
-		// PELA SOMA DOS OUTROS DOIS ANTERIORES. OBSERVE QUE SENDO O METODO
-		// RECURSIVO, O FIBONACCI DOS NUMEROS ANTERIORES TAMBEM VAO SER
-		// IMPRESSOS
+		if (n > 1){
+			result = calcularFibonacci(n-1) + calcularFibonacci(n-2);
+		}
+		
+		return result;
+	}
+
+	private int countNotNull(Object[] array, int indexFrom) {
+		int result = (array[indexFrom] != null) ? 1 : 0;
+		
+		if (indexFrom < array.length - 1) {
+			result = result + countNotNull(array, indexFrom + 1);
+		}
 		return result;
 	}
 
 	public int countNotNull(Object[] array) {
-		int result = 0;
-		// TODO IMPLEMENTE AQUI O CODIGO QUE CONTA (USANDO RECURSAO) A
-		// QUANTIDADE DE ELEMENTOS NAO NULOS
-		// DE UM ARRAY DE OBJETOS RECEBIDO COMO PARAMETRO
-		return result;
+		return countNotNull(array, 0);
 	}
 
 	public long potenciaDe2(int expoente) {
-		int result = 1;
-		// TODO IMPLEMENTE (USANDO RECURSAO) O CODIGO PARA PRODUZIR A N-ESIMA
-		// POTENCIA
-		// DE 2
+		if (expoente < 0) {
+			throw new IllegalArgumentException("Expoente não pode ser negativo");
+		}
+
+		long result = 1;
+		if (expoente != 0) {
+			result = 2 * potenciaDe2(expoente - 1);
+		}
 		return result;
 	}
 
 	public double progressaoAritmetica(double termoInicial, double razao, int n) {
-		double result = 0;
-		// TODO IMPLEMENTE SEU CODIGO (USANDO RECURSAO) DE ENCONTRAR O n-ESIMO
-		// TERMO
-		// DA PROGRESSAO ARITMETICA, DADO O TERMO INICIAL E A RAZAO
-		// VOCE NAO PODE USAR A FORMULA QUE CALCULA O N-ESIMO TERMO. DEVE USAR RECURSAO
+		double result = termoInicial;
+		if (n > 1) {
+			result = progressaoAritmetica(termoInicial, razao, n - 1) + razao;
+		}
 		return result;
 	}
 
 	public double progressaoGeometrica(double termoInicial, double razao, int n) {
-		double result = 1;
-		// TODO IMPLEMENTE SEU CODIGO (USANDO RECURSAO) DE ENCONTRAR O n-ESIMO
-		// TERMO
-		// DA PROGRESSAO GEOMETRICA, DADO O TERMO INICIAL E A RAZAO
-		// VOCE NAO PODE USAR A FORMULA QUE CALCULA O N-ESIMO TERMO. DEVE USAR RECURSAO
+		double result = termoInicial;
+		if (n > 1) {
+			result = progressaoGeometrica(termoInicial, razao, n - 1) * razao;
+		}
 		return result;
 	}
-	
-	
 }
