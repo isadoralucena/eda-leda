95e2b3310fbc5ccd7068f0f9635cd134f318901e
124110308 ISADORA BEATRIZ LUCENA DE MEDEIROS <isadorabeatriz595@gmail.com>
2025-07-16 09:25:20 -0300

    refactor: melhora implementação do floor e ceil

--- R05-01/src/main/java/problems/FloorCeilBinarySearchImpl.java
+++ R05-01/src/main/java/problems/FloorCeilBinarySearchImpl.java
@@ -15,25 +15,51 @@ public class FloorCeilBinarySearchImpl implements FloorCeil {
 
 	@Override
 	public Integer floor(Integer[] array, Integer x) {
-		Integer answer = binarySearchFloor(array, x, 0, array.length - 1);
+		Integer answer = -1;
+		if (array != null && array.length > 0) {
+			answer = binarySearchFloor(array, x, 0, array.length - 1);
+		}
 		return answer;
 	}
 
 	private Integer binarySearchFloor(Integer[] array, Integer x, int left, int right){
-		int middle = (left + right)/2;
-		Integer answer =  null;
-		if(array[middle] <= x){
-			answer = binarySearchFloor(array, x, left -  1, right);
-		} else if (array[middle] > x){
-			answer = binarySearchFloor(array, x, left, right - 1);
+		Integer answer =  -1;
+		if(left >= 0 && right < array.length){
+			while(left <= right){
+				int middle = (left + right)/2;
+				if(array[middle] <= x){
+					answer = array[middle];
+					left = middle + 1;
+				} else {
+					right = middle - 1;
+				}
+			}
 		}
 		return answer;
 	}
 
 	@Override
 	public Integer ceil(Integer[] array, Integer x) {
-		// TODO implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		Integer answer = -1;
+		if (array != null && array.length > 0) {
+			answer = binarySearchCeil(array, x, 0, array.length - 1);
+		}
+		return answer;
 	}
 
+	private Integer binarySearchCeil(Integer[] array, Integer x, int left, int right){
+		Integer answer =  -1;
+		if(left >= 0 && right < array.length){
+			while(left <= right){
+				int middle = (left + right)/2;
+				if(array[middle] >= x){
+					answer = array[middle];
+					right = middle - 1;
+				} else {
+					left = middle + 1;
+				}
+			}
+		}
+		return answer;
+	}
 }
