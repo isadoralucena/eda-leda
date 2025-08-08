64b12b019fc03d1538f55762668b82c3e9d54041
Isadora Lucena <isadorabeatriz595@gmail.com>
2025-08-07 22:26:58 -0300

    feat: add double linked list

--- R08-01/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
+++ R08-01/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
@@ -1,34 +1,113 @@
 package adt.linkedList;
 
-public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
-		DoubleLinkedList<T> {
-
+public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements DoubleLinkedList<T> {
+	
 	protected DoubleLinkedListNode<T> last;
+
+	public DoubleLinkedListImpl(){
+		this.last = new DoubleLinkedListNode<>();
+		this.head = this.last;
+	}
 
 	@Override
 	public void insertFirst(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(element != null){ 
+			DoubleLinkedListNode<T> oldHead = (DoubleLinkedListNode<T>) this.getHead();
+			DoubleLinkedListNode<T> nilNode =  new DoubleLinkedListNode<T>();
+			DoubleLinkedListNode<T> newHead = new DoubleLinkedListNode<T>(element, oldHead, nilNode);
+
+			oldHead.setPrevious(newHead);
+			nilNode.setNext(newHead);
+
+			if(this.getHead().isNIL()){
+				this.setLast(newHead);
+			}
+			this.setHead(newHead);
+		}
 	}
 
 	@Override
 	public void removeFirst() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		DoubleLinkedListNode<T> oldHead = (DoubleLinkedListNode<T>) this.getHead();
+
+		if(!oldHead.isNIL()){
+			DoubleLinkedListNode<T> newHead = (DoubleLinkedListNode<T>) oldHead.getNext();
+			this.setHead(newHead);
+
+			if(newHead.isNIL()){
+				this.setLast(newHead);
+			}else{
+				newHead.setPrevious(new DoubleLinkedListNode<T>());
+			}
+		}
 	}
 
 	@Override
 	public void removeLast() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		DoubleLinkedListNode<T> oldLast = this.getLast();
+
+		if(!oldLast.isNIL()){
+			DoubleLinkedListNode<T> newLast = oldLast.getPrevious();
+			this.setLast(newLast);
+
+			if(newLast.isNIL()){
+				this.setHead(newLast);
+			}else{
+				newLast.setNext(new DoubleLinkedListNode<T>());
+			}
+		}
+	}
+
+	@Override
+	public void remove(T element){
+		if(!this.isEmpty() && element != null){
+			DoubleLinkedListNode<T> auxHead = (DoubleLinkedListNode<T>) this.getHead();
+
+			while(!auxHead.isNIL() && !auxHead.getData().equals(element)){
+				auxHead = (DoubleLinkedListNode<T>) auxHead.getNext();
+			}
+
+			if(!auxHead.isNIL()){
+				DoubleLinkedListNode<T> prevNode = auxHead.getPrevious();
+				DoubleLinkedListNode<T> nextNode = (DoubleLinkedListNode<T>) auxHead.getNext();
+				
+				if(!nextNode.isNIL()){
+					nextNode.setPrevious(prevNode);
+				}else{
+					this.setLast(prevNode);
+				}
+
+				if (!prevNode.isNIL()) {
+					prevNode.setNext(nextNode);
+				} else {
+					this.setHead(nextNode);
+				}
+			}		
+		}
+	}
+
+	@Override
+	public void insert(T element){
+		if(element != null){ 
+			DoubleLinkedListNode<T> nilNode =  new DoubleLinkedListNode<T>();
+			DoubleLinkedListNode<T> oldLastNode = this.getLast();
+			DoubleLinkedListNode<T> newLastNode = new DoubleLinkedListNode<T>(element, nilNode, oldLastNode);
+
+			if(!oldLastNode.isNIL()){
+				oldLastNode.setNext(newLastNode);
+			}else {
+				this.setHead(newLastNode);
+		}
+			nilNode.setPrevious(newLastNode);
+			this.setLast(newLastNode);
+		}
 	}
 
 	public DoubleLinkedListNode<T> getLast() {
-		return last;
+		return this.last;
 	}
 
 	public void setLast(DoubleLinkedListNode<T> last) {
 		this.last = last;
 	}
-
 }
--- R08-01/src/test/java/adt/linkedList/StudentDoubleLinkedListTest.java
+++ R08-01/src/test/java/adt/linkedList/StudentDoubleLinkedListTest.java
@@ -13,39 +13,150 @@ public class StudentDoubleLinkedListTest extends StudentLinkedListTest {
 
 		getImplementations();
 
-		// Lista com 3 elementos.
 		lista1.insert(3);
 		lista1.insert(2);
 		lista1.insert(1);
 
-		// Lista com 1 elemento.
 		lista3.insert(1);
 	}
 
 	private void getImplementations() {
-		// TODO O aluno deve ajustar aqui para instanciar sua implementação
-		lista1 = null;
-		lista2 = null;
-		lista3 = null;
+		lista1 = new DoubleLinkedListImpl<>();
+		lista2 = new DoubleLinkedListImpl<>();;
+		lista3 = new DoubleLinkedListImpl<>();;
 	}
-
-	// Métodos de DoubleLinkedList
 
 	@Test
 	public void testInsertFirst() {
 		((DoubleLinkedList<Integer>) lista1).insertFirst(4);
 		Assert.assertArrayEquals(new Integer[] { 4, 3, 2, 1 }, lista1.toArray());
+
+		((DoubleLinkedList<Integer>) lista2).insertFirst(10);
+		Assert.assertArrayEquals(new Integer[] { 10 }, lista2.toArray());
 	}
 
 	@Test
 	public void testRemoveFirst() {
 		((DoubleLinkedList<Integer>) lista1).removeFirst();
 		Assert.assertArrayEquals(new Integer[] { 2, 1 }, lista1.toArray());
+
+		((DoubleLinkedList<Integer>) lista3).removeFirst();
+		Assert.assertTrue(lista3.isEmpty());
+		Assert.assertArrayEquals(new Integer[] {}, lista3.toArray());
 	}
 
 	@Test
 	public void testRemoveLast() {
 		((DoubleLinkedList<Integer>) lista1).removeLast();
 		Assert.assertArrayEquals(new Integer[] { 3, 2 }, lista1.toArray());
+
+		((DoubleLinkedList<Integer>) lista3).removeLast();
+		Assert.assertTrue(lista3.isEmpty());
+		Assert.assertArrayEquals(new Integer[] {}, lista3.toArray());
 	}
+
+		@Test
+	public void testInsertAndRemoveFirstInEmptyList() {
+		((DoubleLinkedList<Integer>) lista2).insertFirst(99);
+		Assert.assertArrayEquals(new Integer[] { 99 }, lista2.toArray());
+
+		((DoubleLinkedList<Integer>) lista2).removeFirst();
+		Assert.assertTrue(lista2.isEmpty());
+	}
+
+	@Test
+	public void testInsertAndRemoveLastInEmptyList() {
+		((DoubleLinkedList<Integer>) lista2).insert(88);
+		Assert.assertArrayEquals(new Integer[] { 88 }, lista2.toArray());
+
+		((DoubleLinkedList<Integer>) lista2).removeLast();
+		Assert.assertTrue(lista2.isEmpty());
+	}
+
+	@Test
+	public void testInsertFirstMultiple() {
+		DoubleLinkedList<Integer> list = (DoubleLinkedList<Integer>) lista2;
+		list.insertFirst(5);
+		list.insertFirst(4);
+		list.insertFirst(3);
+		list.insertFirst(2);
+		list.insertFirst(1);
+		Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 4, 5 }, list.toArray());
+	}
+
+	@Test
+	public void testRemoveFirstAndLastSequence() {
+		DoubleLinkedList<Integer> list = (DoubleLinkedList<Integer>) lista1;
+		list.removeFirst();
+		list.removeLast();
+		Assert.assertArrayEquals(new Integer[] { 2 }, list.toArray());
+
+		list.removeFirst();
+		Assert.assertTrue(list.isEmpty());
+	}
+
+	@Test
+	public void testRemoveElementMiddle() {
+		DoubleLinkedList<Integer> list = (DoubleLinkedList<Integer>) lista1;
+		list.remove(2);
+		Assert.assertArrayEquals(new Integer[] { 3, 1 }, list.toArray());
+	}
+
+	@Test
+	public void testRemoveFirstElementByValue() {
+		DoubleLinkedList<Integer> list = (DoubleLinkedList<Integer>) lista1;
+		lista1.remove(3);
+		Assert.assertArrayEquals(new Integer[] { 2, 1 }, lista1.toArray());
+	}
+
+	@Test
+	public void testRemoveLastElementByValue() {
+		DoubleLinkedList<Integer> list = (DoubleLinkedList<Integer>) lista1;
+		list.remove(1);
+		Assert.assertArrayEquals(new Integer[] { 3, 2 }, list.toArray());
+	}
+
+	@Test
+	public void testRemoveNonExistentElement() {
+		DoubleLinkedList<Integer> list = (DoubleLinkedList<Integer>) lista1;
+		list.remove(99); 
+		Assert.assertArrayEquals(new Integer[] { 3, 2, 1 }, list.toArray());
+	}
+
+	@Test
+	public void testRemoveFromEmptyList() {
+		DoubleLinkedList<Integer> list = (DoubleLinkedList<Integer>) lista2;
+		list.remove(10);
+		Assert.assertTrue(list.isEmpty());
+	}
+
+	@Test
+	public void testMixedInsertAndRemovals() {
+		DoubleLinkedList<Integer> list = (DoubleLinkedList<Integer>) lista2;
+
+		list.insert(1);
+		list.insertFirst(0);
+		list.insert(2);
+		list.removeFirst(); 
+		list.removeLast();  
+		Assert.assertArrayEquals(new Integer[] { 1 }, list.toArray());
+
+		list.remove(1);
+		Assert.assertTrue(list.isEmpty());
+	}
+	@Test
+	public void testInsertNullDoesNothing() {
+		DoubleLinkedList<Integer> list = (DoubleLinkedList<Integer>) lista2;
+		list.insert(null);
+		list.insertFirst(null);
+		Assert.assertTrue(list.isEmpty());
+	}
+
+	@Test
+	public void testRemoveNullDoesNothing() {
+		DoubleLinkedList<Integer> list = (DoubleLinkedList<Integer>) lista1;
+		list.remove(null);
+		Assert.assertArrayEquals(new Integer[] { 3, 2, 1 }, list.toArray());
+	}
+
 }
