43f876bd3e66c00c2c6bc8bee5ec4bf8ece16740
Isadora Lucena <isadorabeatriz595@gmail.com>
2025-08-07 23:29:35 -0300

    feat: add implementação de stack com double linked list

--- R08-01/src/test/java/adt/stack/StudentStackTest.java
+++ R08-01/src/test/java/adt/stack/StudentStackTest.java
@@ -31,13 +31,11 @@ public class StudentStackTest {
 	}
 
 	private void getImplementations() {
-		// TODO O aluno deve ajustar aqui para instanciar sua implementação
-		stack1 = null;
-		stack2 = null;
+		stack1 = new StackDoubleLinkedListImpl<>(4);
+		stack2 = new StackDoubleLinkedListImpl<>(2);
 		stack3 = null;
 	}
 
-	// MÉTODOS DE TESTE
 	@Test
 	public void testTop() {
 		assertEquals(new Integer(3), stack1.top());
@@ -50,8 +48,7 @@ public class StudentStackTest {
 
 	@Test
 	public void testIsFull() {
-		assertFalse(stack1.isFull()); // vai depender do tamanho que a pilha foi
-										// iniciada!!!!
+		assertFalse(stack1.isFull());
 	}
 
 	@Test
@@ -59,15 +56,14 @@ public class StudentStackTest {
 		try {
 			stack1.push(new Integer(5));
 		} catch (StackOverflowException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	@Test(expected = StackOverflowException.class)
 	public void testPushComErro() throws StackOverflowException {
-		stack1.push(new Integer(5)); // levanta excecao apenas se o tamanhonao
-										// permitir outra insercao
+		stack1.push(new Integer(5));
+		stack1.push(new Integer(5));
 	}
 
 	@Test
@@ -75,14 +71,74 @@ public class StudentStackTest {
 		try {
 			assertEquals(new Integer(3), stack1.pop());
 		} catch (StackUnderflowException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	@Test(expected = StackUnderflowException.class)
 	public void testPopComErro() throws StackUnderflowException {
-		assertEquals(new Integer(3), stack1.pop()); // levanta excecao apenas se
-													// stack1 for vazia
+		assertEquals(new Integer(3), stack1.pop());
+		assertEquals(new Integer(2), stack1.pop());
+		assertEquals(new Integer(1), stack1.pop());
+		assertEquals(new Integer(7), stack1.pop());
 	}
+
+	@Test
+public void testPopEmOrdemCorreta() throws StackUnderflowException {
+	assertEquals(new Integer(3), stack1.pop());
+	assertEquals(new Integer(2), stack1.pop());
+	assertEquals(new Integer(1), stack1.pop());
+	assertTrue(stack1.isEmpty());
+}
+
+@Test
+public void testPushPopAlternado() throws StackOverflowException, StackUnderflowException {
+	stack1.pop();
+	stack1.push(99); 
+	assertEquals(new Integer(99), stack1.top());
+	assertEquals(new Integer(99), stack1.pop());
+	assertEquals(new Integer(2), stack1.top());
+}
+
+@Test
+public void testTopSemModificar() throws StackUnderflowException {
+	Integer top1 = stack1.top();
+	Integer top2 = stack1.top();
+	assertEquals(top1, top2);
+	assertEquals(new Integer(3), top1);
+}
+
+@Test
+public void testIsEmptyNaStack3() {
+	getImplementations();
+	assertTrue(stack3 == null || stack3.isEmpty());
+}
+
+@Test
+public void testIsFullStack2() {
+	assertTrue(stack2.isFull());
+}
+
+@Test(expected = StackOverflowException.class)
+public void testPushStack2Estourando() throws StackOverflowException {
+	stack2.push(99);
+}
+
+@Test
+public void testPushPopTodosElementos() throws StackOverflowException, StackUnderflowException {
+	getImplementations();
+	stack3 = new StackDoubleLinkedListImpl<>(3);
+	assertTrue(stack3.isEmpty());
+
+	stack3.push(10);
+	stack3.push(20);
+	stack3.push(30);
+	assertTrue(stack3.isFull());
+
+	assertEquals(new Integer(30), stack3.pop());
+	assertEquals(new Integer(20), stack3.pop());
+	assertEquals(new Integer(10), stack3.pop());
+	assertTrue(stack3.isEmpty());
+}
+
 }
